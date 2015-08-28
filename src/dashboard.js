b2a58f526d0229c88cb58f1e7f6edd92f5e7036c
Andrew Pong <a@awp.io>
2015-08-28 00:12:30 -0700

    initial commit, dashboard route setup

---
+++ src/dashboard.js
@@ -0,0 +1,30 @@
+import {inject} from 'aurelia-framework';
+import {HttpClient} from 'aurelia-fetch-client';
+import 'fetch';
+
+@inject(HttpClient)
+export class Users{
+  heading = 'Gauging Stations';
+  users = [];
+  gaugingStations = [];
+
+  constructor (http){
+    http.configure(config => {
+      config
+        .useStandardConfiguration()
+        .withBaseUrl('https://piwebapi.dstcontrols.com/piwebapi/');
+    });
+
+    this.http = http;
+  }
+
+  activate (){
+    return this.http.fetch('system/versions')
+      .then(response => response.json())
+      .then(response => console.log(response));
+
+      return this.http.fetch('elements/E0RCRoEQjjJ06jHBU6_gMSNgJ0bXNiDi5BGAyABQVqoXkgRFNULVM1MTU1LVZNXFBDV0FcR0FVR0lORyBTVEFUSU9OUw/elements')
+        .then(response => response.json())
+        .then(response => this.gaugingStations = response);
+  }
+}
